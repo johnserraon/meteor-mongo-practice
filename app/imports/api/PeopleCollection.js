61ec93a1a8b20cbc14cebe60732b85f23f67bf52
John Serraon <jserraon@hawaii.edu>
2024-03-07 11:00:24 -1000

    Completed E43 simple schema
    E43: Experience Simple Schema 3/7

--- app/imports/api/PeopleCollection.js
+++ app/imports/api/PeopleCollection.js
@@ -1,3 +1,22 @@
+// --using vanilla Collection
+// import { Mongo } from 'meteor/mongo';
+//
+// export const PeopleCollection = new Mongo.Collection('People');
+
+// --using Collection2 and simple-schema
 import { Mongo } from 'meteor/mongo';
+import 'meteor/aldeed:collection2/static';
+import SimpleSchema from 'simpl-schema';
+
+const peopleSchema = new SimpleSchema(
+  {
+    first: String,
+    last: String,
+    age: SimpleSchema.Integer,
+    city: String
+  }
+);
 
 export const PeopleCollection = new Mongo.Collection('People');
+
+PeopleCollection.attachSchema(peopleSchema);
